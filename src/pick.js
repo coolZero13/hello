b08a0fedad56a3c56397d0a0b82c0bf1a33aa04f
AR <rafay84@gmail.com>
2014-05-12 20:42:58 -0500

    pick each added

--- src/pick.js
+++ src/pick.js
@@ -1,8 +1,13 @@
 var pick = function(obj, keys){
     var result = {};
-    for(var i = 0; i < keys.length; ++i){
-        if(keys[i] in obj)
-            result[keys[i]] = obj[keys[i]];
-    }
+    each(keys, function(key){
+        if(key in obj)
+            result[key] = obj[key];
+    });
     return result;
+};
+
+var each = function(arr, func){
+    for(var i = 0; i < arr.length; ++i)
+        func(arr[i]);
 };
